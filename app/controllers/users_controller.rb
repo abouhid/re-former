f1656c310fab862db94abe5b0841adae32a8dbd1
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-10 17:42:18 -0300

    edit issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,33 @@ class UsersController < ApplicationController
     @user=User.new
   end
 
+  def edit
+     @user = User.find(params[:user])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def create
     # @user =  User.new(username: params[:username], email: params[:email], password: params[:password])
     @user=User.new(user_params)
     if @user.save
-      redirect_to new_users_url
+      redirect_to @user
     else
       render :new
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
