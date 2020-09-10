7a661a015b3addb30c7e6f5b7101557dd3a1ddf8
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-09-10 20:35:30 +0300

    Create a working HTML User form.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    def new 
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to new_users_url
+        else
+          render :new
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+end
