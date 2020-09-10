e4e773a4242115dc0b74e01769ab543698c8d6b7
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-09-10 20:36:26 +0300

    Fix linter issues.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
-    def new 
-    end
+  def new; end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to new_users_url
-        else
-          render :new
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_users_url
+    else
+      render :new
     end
+  end
 
-    private 
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  private
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
